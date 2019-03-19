c71f85fe40a9381cd88a477dfce16e0f8257e850
Hendrik <drikdoank@gmail.com>
2019-03-19 19:28:36 +0700

    mam

--- step1_sentinel_download.py
+++
@@ -1,8 +0,0 @@
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-
-api = SentinelAPI('didok49', 'sentinel49')
-footprint = geojson_to_wkt(read_geojson('search_polygon.geojson'))
-products = api.query(footprint,
-                     producttype='SLC',
-                     orbitdirection='ASCENDING')
-api.download_all(products)
