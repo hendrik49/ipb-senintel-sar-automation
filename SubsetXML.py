ee16c8f60f235d2b73c63c54834d058b6796d9c9
Hendrik <drikdoank@gmail.com>
2019-04-08 21:25:17 +0700

    data

---
+++ SubsetXML.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import xml.etree.ElementTree as etree
+import config
+
+
+cwd = os.getcwd()
+isFile = os.getcwd() + config.xmlpraprocessresult
+indx = 1
+tree = etree.parse(cwd+'/'+config.xmlpathsubset)
+elem = tree.findall(".//node")
+a=[]
+for prnt in elem:
+    if (prnt.attrib["id"]!="Read"):
+        if (prnt.attrib["id"]=="Write"):
+            break
+        else:
+            a.append(prnt.attrib['id'][:3])
+
+gabs='_'.join(a)
+
+##checkdir
+listOfFiles=os.listdir(isFile)
+xml_dir = cwd+config.xmlprocesspathsubset
+if not os.path.exists(xml_dir):
+    os.makedirs(xml_dir)
+
+dim_dir = cwd+config.xmlpraprocessresultsubset
+if not os.path.exists(dim_dir):
+    os.makedirs(dim_dir)
+    
+for d_file in listOfFiles:
+    (sarfileshortname, extension)  = os.path.splitext(d_file)
+    read_data = isFile+d_file
+    write_data = dim_dir+'subset_'+sarfileshortname+'_'+gabs+'.dim'
+    for entry in elem:
+        try:
+            if (entry.attrib["id"]=="Read"):
+                entry[2][0].text = read_data
+            if (entry.attrib["id"]=="Write"):
+                entry[2][0].text = write_data
+        except:
+            continue
+    tree.write(xml_dir+'sar_subset_'+str(indx)+'.xml')
+    indx = indx + 1
--- config.py
+++ config.py
@@ -8,6 +8,11 @@ producttype='GRD'
 orbitdirection='Descending'
 url = 'https://scihub.copernicus.eu/apihub/' 
 sentineldirpath = "//sentineldata//"
+
 xmlpath="xml/Lampung_GRD_Or_Cal_Spk_TC_dB.xml" 
 xmlprocesspath='/XMLprocess/'
-xmlpraprocessresult='/Praproses Result/'
+xmlpraprocessresult='/Praproses_result/'
+
+xmlpathsubset="subset_xml/Subset_Lampung_test_sitesMedium.xml" 
+xmlprocesspathsubset='/subset_XMLprocess/'
+xmlpraprocessresultsubset='/subset_praproses_result/'
