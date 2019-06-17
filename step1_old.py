de59afb1028721bea3cbc8c9aeba7be42fefee2b
Hendrik <drikdoank@gmail.com>
2019-06-17 15:32:18 +0700

    validate image soze

--- step1_old.py
+++ step1_old.py
@@ -4,10 +4,10 @@ import config
 
 api = SentinelAPI(config.username, config.password)
 footprint = geojson_to_wkt(read_geojson(config.geojson))
-products = api.query(footprint,
-                     date=(config.start_date, config.end_date),
-                     platformname = config.platformname,
-                     producttype=config.producttype,
+products = api.query(
+                     date=('20180101', '20180102'),
+                     platformname='Sentinel-2',
+                     producttype='S2MSI1C',
                      orbitdirection=config.orbitdirection)
 api.download_all(products)
 
